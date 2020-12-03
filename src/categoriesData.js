fa2c4d0f839db170a394c1c86950d10c98fc5008
Ishaan Verma <ishaanverma97@gmail.com>
2020-12-04 00:28:18 +0530

    Created dummy data for categories

---
+++ src/categoriesData.js
@@ -0,0 +1,34 @@
+const categoriesData = [
+  {
+    property: 'gender',
+    displayName: 'Gender',
+    options: [
+      'Men',
+      'Women',
+      'Boys',
+      'Girls'
+    ],
+  },
+  {
+    property: 'category',
+    displayName: 'Categories',
+    options: [
+      'Tshirts',
+      'Tops',
+      'Dresses',
+      'Shirts',
+    ],
+  },
+  {
+    property: 'brand',
+    displayName: 'Brand',
+    options: [
+      'Puma',
+      'Roadster',
+      'H&M',
+      'max',
+    ],
+  }
+];
+
+export default categoriesData;
