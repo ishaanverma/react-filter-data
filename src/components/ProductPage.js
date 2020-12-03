01db43d2fab1ca9efdebcb3afa338c9638f9fb5a
Ishaan Verma <ishaanverma97@gmail.com>
2020-12-04 01:48:00 +0530

    Show error alert

--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -5,6 +5,11 @@ import LoadingIndicator from "./LoadingIndicator";
 const ProductPage = ({ products }) => {
   return (
     <div className="container-fluid">
+      {products.isError &&
+        <div class="alert alert-danger" role="alert">
+          Error Loading Products
+        </div>
+      } 
       <div className="row">
         {products.isLoading ? (
           <LoadingIndicator />
